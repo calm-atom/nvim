d84bfd39e84315b22e9afece937ad6b2a6ee7260
John Buckley <jbuckleyv@gmail.com>
2023-12-05 14:27:36 -0600

    Added clangd lsp for cpp

--- init.lua
+++ init.lua
@@ -438,7 +438,7 @@ require('which-key').register({
 --  If you want to override the default filetypes that your language server will attach to you can
 --  define the property 'filetypes' to the map in question.
 local servers = {
-  -- clangd = {},
+  clangd = {},
   gopls = {},
   -- pylsp = {},
   -- pyright = {},
