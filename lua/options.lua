11753563c775fb41ff7c41c672347cb2026bcf2e
John Buckley <jbuckleyv@gmail.com>
2025-06-27 07:32:08 -0500

    update tabstop and shiftwidth

--- lua/options.lua
+++ lua/options.lua
@@ -63,6 +63,14 @@ vim.o.cursorline = true
 -- Minimal number of screen lines to keep above and below the cursor.
 vim.o.scrolloff = 10
 
+-- Number of columns that make up one level of (auto)indention. 
+-- Default is 8
+vim.o.shiftwidth = 4
+
+-- Column multiple used to display horizontal tab
+-- Default is 8
+vim.o.tabstop = 4
+
 -- if performing an operation that would fail due to unsaved changes in the buffer (like `:q`),
 -- instead raise a dialog asking if you wish to save the current file(s)
 -- See `:help 'confirm'`
