d30f3487679c24ca491c3d73cf2a40fadfbe62b3
John Buckley <jbuckleyv@gmail.com>
2025-06-27 10:22:05 -0500

    add python support

--- lua/kickstart/plugins/lspconfig.lua
+++ lua/kickstart/plugins/lspconfig.lua
@@ -221,6 +221,8 @@ return {
 			local servers = {
 				-- clangd = {},
 				gopls = {},
+				basedpyright = {},
+				ruff = {},
 				-- pyright = {},
 				-- rust_analyzer = {},
 				-- ... etc. See `:help lspconfig-all` for a list of all the pre-configured LSPs
