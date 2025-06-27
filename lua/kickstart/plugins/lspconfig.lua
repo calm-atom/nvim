62e4b6e6555a70aef796330bd70acc2376c47c72
John Buckley <jbuckleyv@gmail.com>
2025-06-27 14:00:00 -0500

    add shellcheck to lsp

--- lua/kickstart/plugins/lspconfig.lua
+++ lua/kickstart/plugins/lspconfig.lua
@@ -223,6 +223,7 @@ return {
 				gopls = {},
 				basedpyright = {},
 				ruff = {},
+				shellcheck = {},
 				-- pyright = {},
 				-- rust_analyzer = {},
 				-- ... etc. See `:help lspconfig-all` for a list of all the pre-configured LSPs
