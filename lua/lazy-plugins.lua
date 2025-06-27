14552d5d157b96f35d47ed80b7fb0b0887491497
John Buckley <jbuckleyv@gmail.com>
2025-06-27 07:46:39 -0500

    add treesitter

--- lua/lazy-plugins.lua
+++ lua/lazy-plugins.lua
@@ -17,6 +17,7 @@ require('lazy').setup({
   require 'kickstart.plugins.gitsigns',
   require 'kickstart.plugins.lspconfig',
   require 'kickstart.plugins.telescope',
+  require 'kickstart.plugins.treesitter',
 
   }, {
   ui = {
