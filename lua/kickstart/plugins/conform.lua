4605b88a4a2b066faa996c58f8a9daec16ccb587
John Buckley <jbuckleyv@gmail.com>
2025-06-27 09:36:20 -0500

    add golang features

--- lua/kickstart/plugins/conform.lua
+++ lua/kickstart/plugins/conform.lua
@@ -30,6 +30,7 @@ return {
         end
       end,
       formatters_by_ft = {
+        go = { 'gofmt', 'goimports', 'golines', 'staticcheck'},
         lua = { 'stylua' },
         -- Conform can also run multiple formatters sequentially
         -- python = { "isort", "black" },
