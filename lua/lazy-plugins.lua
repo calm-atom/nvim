350118f27db63c8023dffa44279947892ba19947
John Buckley <jbuckleyv@gmail.com>
2025-09-26 07:53:37 -0500

    add harpoon

--- lua/lazy-plugins.lua
+++ lua/lazy-plugins.lua
@@ -24,6 +24,7 @@ require("lazy").setup({
 	require("kickstart.plugins.todo-comments"),
 	-- require("kickstart.plugins.duskfox"),
 	require("kickstart.plugins.everforest"),
+	require("kickstart.plugins.harpoon"),
 }, {
 	ui = {
 		-- If you are using a Nerd Font: set icons to an empty table which will use the
