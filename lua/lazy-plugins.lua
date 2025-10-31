5b021551492193b69fc6d18066b5d2cf2366e1bb
John Buckley <jbuckleyv@gmail.com>
2025-10-31 16:13:13 -0500

    feat: add neogit

--- lua/lazy-plugins.lua
+++ lua/lazy-plugins.lua
@@ -26,6 +26,7 @@ require("lazy").setup({
 	require("kickstart.plugins.harpoon"),
 	require("kickstart.plugins.osc52"),
 	require("kickstart.plugins.flash"),
+	require("kickstart.plugins.neogit"),
 }, {
 	ui = {
 		-- If you are using a Nerd Font: set icons to an empty table which will use the
